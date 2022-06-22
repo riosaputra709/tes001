3939271616a5f984acf3df26005c5681af91038b
Rio Saputra <riosaputra709@gmail.com>
2022-06-22 15:19:29 +0700

    part 3

---
+++ app/src/main/java/com/example/androiduberremake/Common.java
@@ -0,0 +1,5 @@
+package com.example.androiduberremake;
+
+public class Common {
+    public static final String DRIVER_INFO_REFERENCE = "DriverInfo";
+}
---
+++ app/src/main/java/com/example/androiduberremake/Model/DriverInfoModel.java
@@ -0,0 +1,41 @@
+package com.example.androiduberremake.Model;
+
+public class DriverInfoModel {
+    private String firstName, lastName, PhoneNumber;
+    private double rating;
+
+    public DriverInfoModel() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String latName) {
+        this.lastName = latName;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
